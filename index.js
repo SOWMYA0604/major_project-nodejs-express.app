03086bca367698aaf533bdddaf21213a5580ad97
SOWMYA0604 <20wh1a12b4@bvrithyderabad.edu.in>
2022-12-04 17:19:22 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const e = require("express");
+const express = require("express");
+const math=require("mathjs");
+const app = express();
+//const port = 3000;
 
 const { initializeApp, cert } = require('firebase-admin/app');
 //const { getFirestore} = require('firebase-admin/firestore');
-var express = require('express')  
-var app = express()  
+//var express = require('express')  
+//var app = express()  
 const port=3000;
 //const { initializeApp } = require("firebase-admin/app");
 const { getFirestore}=require('firebase-admin/firestore');
@@ -12,6 +17,7 @@ initializeApp({
 });
 const db=getFirestore();
 app.set('view engine','ejs');
+app.use(express.static('public'));
  app.get("/signin",function(req,res){
  	res.render("signin");
  });
@@ -30,7 +36,7 @@ app.set('view engine','ejs');
 	.then((docs) => {
         //console.log(docs.size);
 		if(docs.size > 0){
-			res.send("Login Succesfull");
+			res.render("Home");
 		}
 		else{
 			res.send("Login Failed");
@@ -48,8 +54,57 @@ app.get("/signupsubmit",(req,res)=>{
 		Email : Email,
 		password: password,
 	}).then(()=>{
-		res.send("sign up succesfull");
+		res.render("signin");
 	});
+});
+
+const arr=[];
+const costs=[];
+var amount=0;
+app.get("/add_adv_ToCart",(req,res)=>{
+	const val=req.query.item;
+	var c=req.query.cost;
+	costs.push(c);
+	c=math.evaluate(c.slice(0,c.length-2));
+	amount=amount+c;
+	arr.push(val);
+	res.render("home");
+});
+app.get("/add_myth_ToCart",(req,res)=>{
+	const val=req.query.item;
+	var c=req.query.cost;
+	costs.push(c);
+	c=math.evaluate(c.slice(0,c.length-2));
+	amount=amount+c;
+	arr.push(val);
+	res.render("home2");
+});
+app.get("/add_k_ToCart",(req,res)=>{
+	const val=req.query.item;
+	var c=req.query.cost;
+	costs.push(c);
+	c=math.evaluate(c.slice(0,c.length-2));
+	amount=amount+c;
+	arr.push(val);
+	res.render("home3");
+});
+
+app.get("/cart",(req,res)=>{
+	if(typeof(arr) != "undefined"){
+		db.collection("Cart").add({
+			Cart : arr,
+			Costs : costs,
+			TotalCost : amount,
+		}).then(()=>{
+			res.render("cart",{booksData : arr, amount : amount, costs : costs});
+		});
+	}
+});
+app.get("/Home",function(req,res){
+	res.render("Home2");
+});
+app.get("/Home2",function(req,res){
+	res.render("Home3");
 });
 app.listen(3000, function () {  
 console.log('Example app listening on port 3000!')  
